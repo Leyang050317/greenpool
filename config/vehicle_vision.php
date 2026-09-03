df2b42029fe9ab5f02d5505c7d40a48f54ee5c93
Anduuu05 <andrewyyy0520@gmail.com>
2026-09-04 01:51:12 +0800

    fix: install OCR runtime on Railway

--- config/vehicle_vision.php
+++ config/vehicle_vision.php
@@ -2,7 +2,7 @@
 
 return [
     'enabled' => (bool) env('LOCAL_VEHICLE_VISION_ENABLED', env('APP_ENV') !== 'testing'),
-    'node_binary' => env('LOCAL_VEHICLE_VISION_NODE', 'C:\\Program Files\\nodejs\\node.exe'),
+    'node_binary' => env('LOCAL_VEHICLE_VISION_NODE', 'node'),
     'worker' => base_path('vision/vehicle-image-classifier.mjs'),
     'model' => env('LOCAL_VEHICLE_VISION_MODEL', 'Xenova/clip-vit-base-patch32'),
     'cache_directory' => storage_path('app/models/transformers'),
