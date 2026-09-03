7d70244503865f9a15f4010c8d9a8fd375992364
Anduuu05 <andrewyyy0520@gmail.com>
2026-09-03 22:13:19 +0800

    fix: trust Railway proxy for HTTPS asset URLs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         __DIR__.'/../app/Console/Commands',
     ])
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->validateCsrfTokens(except: [
             'stripe/webhook',
         ]);
